50e0a45b47c3ece3a3e586dc94d5e1770ff777fd
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-08-02 02:53:58 -0400

    cleaned git Links

--- app/projects/sections/GamesTab.tsx
+++ app/projects/sections/GamesTab.tsx
@@ -6,8 +6,8 @@ const GamesTab = () => {
     <div className="my-10  grid grid-cols-1 lg:grid-cols-2 gap-3 text-white">
       <Card
         title="Ball Havoc"
-        link="https://gummyjohn.github.io/ball-havoc/"
-        gitLink="https://github.com/GummyJohn/ball-havoc"
+        link="https://j0hnnyli.github.io/ball-havoc/"
+        gitLink="https://github.com/j0hnnyli/ball-havoc?tab=readme-ov-file"
         img="/ballhavoc.png"
         description="Interactive challenge where players tries to stay alive"
         tags={["HTML", "CSS", "Canvas", "Javascript"]}
